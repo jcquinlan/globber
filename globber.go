1dccc43e8397bb2279a47ef132fc40037524c36c
jcquinlan <jcquinlan.dev@gmail.com>
2019-04-07 14:01:55 +0200

    styling and hidden file handling

--- globber.go
+++ globber.go
@@ -6,14 +6,17 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 var glob *string
 var root *string
+var includeHiddenFiles *bool
 
 func main() {
 	glob = flag.String("glob", "*", "The glob pattern that will be used to match filenames")
 	root = flag.String("root", ".", "The starting directory that will act as the root")
+	includeHiddenFiles = flag.Bool("hidden-files", false, "Whether or not to include hidden directories and files")
 	flag.Parse()
 
 	digestDir(*root, 0)
@@ -28,20 +31,31 @@ func digestDir(path string, depth uint8) {
 	}
 
 	for _, item := range fileInfo {
+		itemName := item.Name()
+		styledPath := itemName
+
+		if !*includeHiddenFiles && strings.HasPrefix(itemName, ".") {
+			continue
+		}
+
 		// Recursively handle subdirectories
 		if item.IsDir() {
+			styledPath = indentPath(styledPath, depth)
+			fmt.Println(styledPath)
 			digestDir(buildPathName(path, item), depth+1)
-
 		} else {
-			matched, err := filepath.Match(*glob, item.Name())
+			matched, err := filepath.Match(*glob, itemName)
 
 			if err != nil {
 				panic(err)
 			}
 
 			if matched {
-				adornPath(buildPathName(path, item), depth)
+				styledPath = colorPath(styledPath)
 			}
+
+			styledPath = indentPath(styledPath, depth)
+			fmt.Println(styledPath)
 		}
 	}
 }
@@ -50,15 +64,25 @@ func buildPathName(path string, file os.FileInfo) string {
 	return path + "/" + file.Name()
 }
 
-func adornPath(path string, depth uint8) {
-	indentation := buildIndentation(depth)
-	fmt.Println(indentation + path)
+func indentPath(path string, depth uint8) string {
+	return buildIndentation(depth) + path
+}
+
+func colorPath(path string) string {
+	return path
 }
 
 func buildIndentation(depth uint8) string {
 	var spaces string
-	for i := uint8(0); i < depth*4; i++ {
+	depthMagnitude := depth * 4
+	for i := uint8(0); i < depthMagnitude; i++ {
 		spaces = spaces + " "
+	}
+
+	if depth > 0 {
+		spaces = spaces + "\u2514 "
+	} else {
+		spaces = spaces + "- "
 	}
 
 	return spaces
